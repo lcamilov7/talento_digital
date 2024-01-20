18537466d2d84f5a464f0a4570dd0bacb5759b63
Luis Camilo <luiscamilov@hotmail.com>
2024-01-20 18:54:07 -0500

    asociaciones

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,13 @@
 class Client < ApplicationRecord
   belongs_to :user
+  has_many :conversations
+
+  has_many :client_interests
+  has_many :interests, through: :client_interests
+
+  enum status: {
+    "Prospecto" => 0,
+    "Muy interesado" => 1,
+    "Cliente" => 2
+  }
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,2 +1,4 @@
 class Interest < ApplicationRecord
+  has_many :client_interests
+  has_many :clients, thorugh: :client_interests
 end
