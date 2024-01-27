5056a267270c9830a1237df6c897140fa5734ee4
Luis Camilo <luiscamilov@hotmail.com>
2024-01-27 10:45:32 -0500

    errores extranos

--- app/models/client_interest.rb
+++
@@ -1,4 +0,0 @@
-class ClientInterest < ApplicationRecord
-  belongs_to :client
-  belongs_to :interest
-end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap" # @5.3.2
-# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+# pin "bootstrap" # @5.3.2
 pin "slim-select" # @2.8.1
+# pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
